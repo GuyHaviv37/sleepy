7a66d1a3576e77178445f4ce07e576c0ff5bc660
GuyHaviv37 <guyhavivcodes@gmail.com>
2022-08-01 20:38:00 +0300

    add swr

--- src/pages/user/[id]/settings.tsx
+++ src/pages/user/[id]/settings.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
+import useSWR from 'swr';
+import { fetcher } from '@/utils/fetcher';
 
 const SPORT = 'nfl';
 const SEASON = '2021'; // @TODO: turn to 2022
@@ -10,25 +12,28 @@ type LeagueData = {
     league_id: string;
 }
 
-const getSleeperUserLeagues = async (sleeperId: string): Promise<LeagueData[]> => {
-    return await (await fetch(`https://api.sleeper.app/v1/user/${sleeperId}/leagues/${SPORT}/${SEASON}`)).json();
+type UseSleeperUserLeaguesResult = {
+    leagues: LeagueData[],
+    isLoading: boolean,
+    isError: boolean
+}
+
+const useSleeperUserLeagues = (sleeperId: string): UseSleeperUserLeaguesResult => {
+    const {data, error} = useSWR(`https://api.sleeper.app/v1/user/${sleeperId}/leagues/${SPORT}/${SEASON}`, fetcher);
+
+    return {
+        leagues: data,
+        isLoading: !error && !data,
+        isError: error,
+    }
 }
 
 const UserDashboardPage = () => {
     const router = useRouter();
     const { id } = router.query;
-    const [leagues, setLeagues] = useState<LeagueData[] | undefined>();
+    const {isLoading, leagues} = useSleeperUserLeagues(id as string);
 
-    useEffect(() => {
-        const fetchLeagues = async () => {
-            if (typeof id === 'string') {
-                const fetchedLeagues = await getSleeperUserLeagues(id);
-                console.log('fetchedLeagues', fetchedLeagues);
-                setLeagues(fetchedLeagues);
-            }
-        }
-        fetchLeagues();
-    }, [id])
+    console.log('useSleeperUserLeagues', isLoading, leagues);
 
     return (
         <>
@@ -40,10 +45,10 @@ const UserDashboardPage = () => {
             <main className="container mx-auto flex flex-col items-center justify-center h-screen p-4 bg-background-main">
                 <section className='bg-primary w-4/5 min-w-min rounded-lg px-6 py-4'>
                     <h3 className='text-primary-text text-lg tracking-wider leading-relaxed'>&#x2699; Settings</h3>
-                    {!leagues ? <div>Loading...</div> : (
+                    {isLoading ? <div>Loading...</div> : (
                         <div>
                             Leagues:
-                            {leagues.map((league) => <div>{league.name}</div>)}
+                            {leagues.map((league) => <div key={league.league_id}>{league.name}</div>)}
                         </div>
                     )}
                 </section>
