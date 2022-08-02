328d225fe4b39252218fc90dbff7538e15a51f41
GuyHaviv37 <guyhavivcodes@gmail.com>
2022-08-02 21:26:43 +0300

    add createUser, updateUserScales db mutations

--- src/pages/user/[id]/settings.tsx
+++ src/pages/user/[id]/settings.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import useSWR from 'swr';
 import { fetcher } from '@/utils/fetcher';
+import { inferMutationInput, trpc } from '@/utils/trpc';
 
 const SPORT = 'nfl';
 const SEASON = '2021'; // @TODO: turn to 2022
@@ -28,12 +29,29 @@ const useSleeperUserLeagues = (sleeperId: string): UseSleeperUserLeaguesResult =
     }
 }
 
+// type UpdateUserScalesInput = inferMutationInput<'example.updateUserScales'>;
+
 const UserDashboardPage = () => {
     const router = useRouter();
     const { id } = router.query;
     const {isLoading, leagues} = useSleeperUserLeagues(id as string);
+    const inputRefs = useRef<HTMLInputElement[]>(new Array());
+    const updateUserScales = trpc.useMutation('example.updateUserScales');
 
-    console.log('useSleeperUserLeagues', isLoading, leagues);
+    console.log('input refs: ', inputRefs);
+
+    const submitWeightsHandler = () => {
+        console.log('input Refs: ', inputRefs.current);
+        const leagueWeights: inferMutationInput<'example.updateUserScales'>['scales'] = [];
+        for (let i=0; i < leagues.length; i++) {
+            leagueWeights.push({
+                leagueId: leagues[i]?.league_id ?? '',
+                scale: parseInt(inputRefs.current[i]?.value ?? '0'),
+            })
+        }
+        console.log('leagueWeights: ', leagueWeights);
+        updateUserScales.mutate({scales: leagueWeights, sleeperId: id as string});
+    };
 
     return (
         <>
@@ -43,12 +61,27 @@ const UserDashboardPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className="container mx-auto flex flex-col items-center justify-center h-screen p-4 bg-background-main">
-                <section className='bg-primary w-4/5 min-w-min rounded-lg px-6 py-4'>
-                    <h3 className='text-primary-text text-lg tracking-wider leading-relaxed'>&#x2699; Settings</h3>
+                <section className='bg-primary w-4/5 min-w-min rounded-lg py-4'>
+                    <h3 className='text-primary-text text-xl tracking-wider leading-relaxed px-6'>&#x2699; Settings</h3>
+                    <br/>
                     {isLoading ? <div>Loading...</div> : (
-                        <div>
-                            Leagues:
-                            {leagues.map((league) => <div key={league.league_id}>{league.name}</div>)}
+                        <div className="flex flex-col center-items">
+                            <h5 className="text-primary-text text-lg px-6">Your Leagues:</h5>
+                            <div className="px-2 mb-3">
+                                <p className='text-green-500 text-right'>By $</p>
+                                {leagues.map((league, index) => (
+                                    <LeagueWeightInput
+                                    key={league.league_id}
+                                    leagueName={league.name}
+                                    onInputRef={(element: HTMLInputElement) => inputRefs.current[index] = element}
+                                    />
+                                    ))}
+                            </div>
+                            <button className="text-primary-text rounded-md bg-accent mx-auto px-3 py-1
+                            hover:-translate-y-1 active:translate-y-0"
+                            onClick={submitWeightsHandler}>
+                                Submit
+                            </button>
                         </div>
                     )}
                 </section>
@@ -56,5 +89,23 @@ const UserDashboardPage = () => {
         </>
     )
 };
+
+interface LeagueWeightInputProps {
+    leagueName: string;
+    onInputRef: (element: HTMLInputElement) => void;
+}
+
+const LeagueWeightInput: React.FC<LeagueWeightInputProps> = (props) => {
+    const {leagueName, onInputRef} = props;
+
+    return (
+        <div className="flex justify-between mt-3">
+            <p className="text-primary-text">{leagueName}</p>
+            <input type="number" ref={onInputRef}
+            defaultValue={0} step={1} min={0}
+            className="max-w-[50px] h-full text-center"/>
+        </div>
+    )
+}
 
 export default UserDashboardPage;
--- src/server/router/example.ts
+++ src/server/router/example.ts
@@ -13,4 +13,44 @@ export const exampleRouter = createRouter()
         greeting: `Hello ${input?.text ?? "world"}`,
       };
     },
+  })
+  .mutation('createUser', {
+    input: z.object({
+      sleeperId: z.string(),
+      username: z.string(),
+    }),
+    async resolve({input}) {
+      const createdUser = await prisma?.user.create({
+        data: {
+          sleeperId: input.sleeperId,
+          username: input.username,
+        }
+      });
+      console.log('Created user:', createdUser);
+      return {success: true, createdUser};
+    }
+  })
+  .mutation('updateUserScales', {
+    input: z.object({
+      sleeperId: z.string(),
+      scales: z.array(z.object({
+        scale: z.number(),
+        leagueId: z.string(),
+      })),
+    }),
+    async resolve({input}) {
+      const {sleeperId, scales} = input;
+      console.log('sleeperId', sleeperId);
+      console.log('scales:', scales);
+      const updatedUser = await prisma?.user.update({
+        where: {
+          sleeperId,
+        },
+        data: {
+          scales,
+        }
+      })
+      console.log('Updated User: ', updatedUser);
+      return {success: true, updatedUser};
+    }
   });
