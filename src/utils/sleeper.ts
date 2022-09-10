fc6c6b3d00739c8f881681c1a6a86a6fcfbb6453
GuyHaviv37 <guyhavivcodes@gmail.com>
2022-09-10 23:41:25 +0300

    handle missing matchup data

--- src/utils/sleeper.ts
+++ src/utils/sleeper.ts
@@ -30,13 +30,13 @@ export const extractUserLeagueRosterIds = (rostersData: any, sleeperId: string):
 const isUserMatchup = (rosterId?: string) => (matchup: LeagueMatchup) => matchup.roster_id === rosterId;
 const isOppMatchup = (matchupId?: string, userRosterId?: string) => (matchup: LeagueMatchup) => matchup.matchup_id === matchupId && matchup.roster_id !== userRosterId;
 
-const extractStartersData = (matchups: LeagueMatchupWithLeagueId[]) => {
+const extractStartersData = (matchups: Partial<LeagueMatchupWithLeagueId>[]) => {
     const starterData: Starters = {};
     if (!matchups || matchups.length === 0) return;
     for (let leagueMatchup of matchups) {
-        const leagueId = leagueMatchup.leagueId;
-        leagueMatchup.starters.forEach((starter: LeagueMatchup['starters'][0], index: number) => {
-            const starterScore = leagueMatchup.starters_points[index] ?? 0;
+        const leagueId = leagueMatchup.leagueId ?? '';
+        leagueMatchup?.starters?.forEach((starter: LeagueMatchup['starters'][0], index: number) => {
+            const starterScore = leagueMatchup?.starters_points?.[index] ?? 0;
             starterData[starter] = {
                 ...starterData[starter],
                 leagues: {
@@ -55,7 +55,7 @@ export const extractSleeperMatchupData = (leagueMatchupsData: {[leagueId: string
 : {userStarters?: Starters; oppStarters?: Starters} => {
     const userMatchups = Object.entries(leagueMatchupsData).map(([leagueId, leagueMatchups]) => {
         const userMatchup = leagueMatchups.find(isUserMatchup(leagueRosterIds[leagueId]));
-        if (!userMatchup) throw Error();
+        if (!userMatchup) return {leagueId, matchup_id: 'N/A'};
         return {...userMatchup, leagueId}
     });
     const userStarters = extractStartersData(userMatchups);
@@ -63,7 +63,7 @@ export const extractSleeperMatchupData = (leagueMatchupsData: {[leagueId: string
         const matchupId = userMatchups[index]?.matchup_id;
         const userRosterId = leagueRosterIds[leagueId];
         const oppMatchup = leagueMatchups.find(isOppMatchup(matchupId, userRosterId));
-        if (!oppMatchup) throw Error();
+        if (!oppMatchup) return {leagueId, matchup_id: 'N/A'};
         return {...oppMatchup, leagueId};
     })
     const oppStarters = extractStartersData(oppMatchups);
