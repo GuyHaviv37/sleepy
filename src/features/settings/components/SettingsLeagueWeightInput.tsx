e3101d321f644982cd4497ca089fe59653d6e118
GuyHaviv37 <guyhavivcodes@gmail.com>
2023-06-02 13:24:01 +0300

    settings - mobile

--- src/features/settings/components/SettingsLeagueWeightInput.tsx
+++ src/features/settings/components/SettingsLeagueWeightInput.tsx
@@ -9,22 +9,24 @@ interface LeagueWeightInputProps {
 }
 
 const LeagueWeightInput: React.FC<LeagueWeightInputProps> = (props) => {
-    const {leagueName, onWeightValueHandler, onCheckboxTickHandler, weightValue, checkboxValue} = props;
+    const { leagueName, onWeightValueHandler, onCheckboxTickHandler, weightValue, checkboxValue } = props;
 
     return (
         <div className="flex justify-between mt-3 sm:space-x-5">
-            <input type='checkbox' checked={checkboxValue} onChange={onCheckboxTickHandler}/>
-            <p className="text-primary-text">{leagueName}</p>
-            <div className="flex space-x-3">
+            <input type='checkbox'
+                className='w-4 checked:accent-alt rounded-lg'
+                checked={checkboxValue} onChange={onCheckboxTickHandler} />
+            <p className="text-primary-text text-lg line-clamp-1">{leagueName}</p>
+            <div className="flex space-x-2">
                 <input type="number" onChange={onWeightValueHandler}
-                step={1} min={0} value={weightValue}
-                className="max-w-[50px] h-full text-center rounded-lg text-grey-700
+                    step={1} min={0} value={weightValue}
+                    className="max-w-[40px] max-h-[40px] text-center text-grey-700
                 border-[3px] border-solid border-grey-300
                 transition ease-in-out
-                focus:text-primary focus:border-accent focus:outline-none
+                focus:text-primary focus:border-alt focus:border-[2px] focus:outline-none
                 md:text-md"
                 />
-                <p className="text-green-500">$</p>
+                <p className="text-alt">$</p>
             </div>
         </div>
     )
--- src/pages/user/[id]/settings.tsx
+++ src/pages/user/[id]/settings.tsx
@@ -8,6 +8,7 @@ import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { SleeperLeagueData } from '@/features/leagues/leagues.types';
 import AppHeader from '@/components/layout/AppHeader';
 import { useSettings } from '@/features/settings/useSettings';
+import FlexibleContainer from '@/components/layout/FlexibleContainer';
 
 type UserDashboardPageProps = InferGetServerSidePropsType<typeof getServerSideProps>;
 
@@ -15,7 +16,7 @@ const UserDashboardPage = ({ leagues }: UserDashboardPageProps) => {
     const router = useRouter();
     const { id, fromLogin } = router.query;
     const { leagueIgnoresMap, leagueWeightsMap, shouldShowMissingStarters,
-    onChangeLeagueIgnore, onChangeLeagueWeight, onChangeShowMissingStarters } = useSettings(leagues);
+        onChangeLeagueIgnore, onChangeLeagueWeight, onChangeShowMissingStarters } = useSettings(leagues);
 
     // move outside of component
     const submitWeightsHandler = () => {
@@ -38,55 +39,95 @@ const UserDashboardPage = ({ leagues }: UserDashboardPageProps) => {
 
     return (
         <>
-            <AppHeader title={'Sleepy - Settings'}/>
-            <main className="container mx-auto flex flex-col items-center justify-center h-screen p-4 bg-background-main">
-                <section className='bg-primary w-4/5 min-w-min rounded-lg py-4'>
-                    <div className='flex justify-between px-4'>
-                        <h3 className='text-primary-text text-xl tracking-wider leading-relaxed'>
-                            &#x2699; Settings (WIP)
-                        </h3>
-                        <Link href={`/user/${id}`}>
-                            <button className="text-primary-text text-xl lg:text-2xl">&times;</button>
-                        </Link>
-                    </div>
-                    <br />
-                    <div className="flex flex-col center-items">
-                        <h5 className="text-primary-text text-lg px-6 sm:text-center underline">Your Leagues:</h5>
-                        <div className="px-2 mb-3 max-h-96 overflow-y-auto md:self-center">
-                            {leagues?.map((league) => (
-                                <LeagueWeightInput
-                                    key={league.league_id}
-                                    leagueName={league.name}
-                                    weightValue={leagueWeightsMap[league.league_id] ?? 0}
-                                    onWeightValueHandler={event => onChangeLeagueWeight(league.league_id, parseInt(event.target.value))}
-                                    onCheckboxTickHandler={event => onChangeLeagueIgnore(league.league_id, event.target.checked)}
-                                    checkboxValue={leagueIgnoresMap[league.league_id] ?? true}
-                                />
-                            ))}
-                            <div className='border-t-2 mt-3 border-text-primary-text' />
-                            <div className='flex mt-3'>
-                                <input type='checkbox' checked={shouldShowMissingStarters}
-                                    onChange={event => onChangeShowMissingStarters(event.target.checked)} />
-                                <p className="text-primary-text text-sm pl-5">Show missing starters notice</p>
-                            </div>
-                        </div>
-                        <button className="text-primary-text rounded-md bg-accent mx-auto px-3 py-1
-                            hover:-translate-y-1 active:translate-y-0"
-                            onClick={submitWeightsHandler}>
-                            Submit
-                        </button>
-                        {fromLogin && <button className="px-1 mt-2 text-accent text-xs tracking-wide md:text-base"
-                            onClick={submitWeightsHandler}>
-                            or skip for later
-                        </button>}
-                    </div>
-                    <p className='text-primary-text text-xs px-6 mt-3 font-thin max-w-sm mx-auto lg:text-sm'>
-                        <span className='font-semibold'>You can ignore a league by unticking it&apos; checkbox.</span>
+            <AppHeader title={'Sleepy - Settings'} />
+            <main className="flex flex-col items-center justify-center h-screen p-4 bg-primary">
+                <h3 className="text-xl md:text-2xl text-primary-text font-bold
+                absolute top-5 left-3"
+                >
+                    {fromLogin ? '🏈 Sleepy' : `⚙️ Settings`}
+                </h3>
+                {fromLogin ? null :
+                    <Link href={`/user/${id}`}>
+                        <button className="text-primary-text text-2xl lg:text-2xl absolute top-5 right-5">&times;</button>
+                    </Link>}
+                <FlexibleContainer>
+                    <p className='text-primary-text text-2xl px-6 mt-3 mx-auto lg:text-4xl'>
+                        Enter your leagues entry fees so you can scale better to who you should root for and against.
                         <br />
-                        Here you can enter your leagues entry fees, this will be used
-                        to scale better who you should root for and against.
+                        <span className='text-alt text-sm font-thin'>Ignore a league by unticking its checkbox</span>
                     </p>
-                </section>
+                    <section className='flex flex-col space-y-3 bg-accent px-5 py-3 rounded-lg
+                        md:w-1/2 md:max-w-md'>
+                        <h5 className='text-primary-text text-xl font-semibold tracking-wide'>Your Leagues</h5>
+                        <div className="flex flex-col center-items">
+                            <div className="px-2 mb-3 max-h-96 overflow-y-auto md:self-center">
+                                {leagues?.map((league) => (
+                                    <LeagueWeightInput
+                                        key={league.league_id}
+                                        leagueName={league.name}
+                                        weightValue={leagueWeightsMap[league.league_id] ?? 0}
+                                        onWeightValueHandler={event => onChangeLeagueWeight(league.league_id, parseInt(event.target.value))}
+                                        onCheckboxTickHandler={event => onChangeLeagueIgnore(league.league_id, event.target.checked)}
+                                        checkboxValue={leagueIgnoresMap[league.league_id] ?? true}
+                                    />
+                                ))}
+                                <div className='border-t-[1px] mt-3 border-alt' />
+                                <div className='flex mt-3'>
+                                    <input type='checkbox'
+                                        className='w-4 checked:accent-alt rounded-lg'
+                                        checked={shouldShowMissingStarters}
+                                        onChange={event => onChangeShowMissingStarters(event.target.checked)} />
+                                    <p className="text-primary-text text-sm pl-5">Show missing starters notice</p>
+                                </div>
+                            </div>
+                            <button className="text-primary-text rounded-lg bg-alt w-full py-3"
+                                onClick={submitWeightsHandler}>
+                                Submit
+                            </button>
+                            {fromLogin && <button className="px-1 mt-2 text-primary-text mx-auto text-sm tracking-wide md:text-base w-fit"
+                                onClick={submitWeightsHandler}>
+                                or skip for later
+                            </button>}
+                        </div>
+                    </section>
+                    {/* <section className='bg-primary rounded-lg py-4'>
+                        <div className='flex justify-between px-4'>
+
+                        </div>
+                        <br />
+                        <div className="flex flex-col center-items">
+                            <h5 className="text-primary-text text-lg px-6 sm:text-center underline">Your Leagues:</h5>
+                            <div className="px-2 mb-3 max-h-96 overflow-y-auto md:self-center">
+                                {leagues?.map((league) => (
+                                    <LeagueWeightInput
+                                        key={league.league_id}
+                                        leagueName={league.name}
+                                        weightValue={leagueWeightsMap[league.league_id] ?? 0}
+                                        onWeightValueHandler={event => onChangeLeagueWeight(league.league_id, parseInt(event.target.value))}
+                                        onCheckboxTickHandler={event => onChangeLeagueIgnore(league.league_id, event.target.checked)}
+                                        checkboxValue={leagueIgnoresMap[league.league_id] ?? true}
+                                    />
+                                ))}
+                                <div className='border-t-2 mt-3 border-text-primary-text' />
+                                <div className='flex mt-3'>
+                                    <input type='checkbox' checked={shouldShowMissingStarters}
+                                        onChange={event => onChangeShowMissingStarters(event.target.checked)} />
+                                    <p className="text-primary-text text-sm pl-5">Show missing starters notice</p>
+                                </div>
+                            </div>
+                            <button className="text-primary-text rounded-md bg-accent mx-auto px-3 py-1
+                            hover:-translate-y-1 active:translate-y-0"
+                                onClick={submitWeightsHandler}>
+                                Submit
+                            </button>
+                            {fromLogin && <button className="px-1 mt-2 text-accent text-xs tracking-wide md:text-base"
+                                onClick={submitWeightsHandler}>
+                                or skip for later
+                            </button>}
+                        </div>
+                    </section> */}
+                </FlexibleContainer>
+
             </main>
         </>
     )
