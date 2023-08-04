d94618c504d6e515c56405f7e71b56f3219132b5
GuyHaviv37 <guyhavivcodes@gmail.com>
2023-08-04 19:11:38 +0300

    remove comments

--- src/features/missing-players/MissingPlayersNotice.tsx
+++ src/features/missing-players/MissingPlayersNotice.tsx
@@ -13,17 +13,6 @@ const MissingPlayersNotice: React.FC = () => {
     const { userLeagueInfo: userStarters, playersInfo, scheduleData } = useContext(DashboardContext);
     const { diffInStarters, isSomeDiff } = useMemo(() => getMissingPlayersDiff({ userStarters, playersInfo, leagueStarterSpots, scheduleData }), [leagueStarterSpots]);
 
-    // return shouldShowMissingStarters && isSomeDiff ? (
-    //     <div className='mb-4 text-primary-text bg-rose-500 rounded p-2'>
-    //         <h5 className='text-sm md:text-base'>⚠️ Notice !</h5>
-    //         <p className='text-xs md:text-sm mb-2'>You got some missing starters in your lineups:</p>
-    //         {Object.entries(diffInStarters).map(([leagueId, diff]) => {
-    //             return (diff !== 0) ?
-    //                 <li className='text-sm md:text-base list-none' key={leagueId}>{leagueNames?.[leagueId]} - {diff}</li>
-    //                 : null;
-    //         })}
-    //     </div>
-    // ) : null;
     return shouldShowMissingStarters && isSomeDiff ? (
         <Tooltip TooltipComponent={
             <div className='text-primary-text bg-rose-500 rounded p-2 w-36 md:w-56 text-xs lg:text-sm'>
