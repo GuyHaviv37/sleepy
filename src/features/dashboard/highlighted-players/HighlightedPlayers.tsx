e0478d734e1614530075637237b49dd68787be4b
GuyHaviv37 <guyhavivcodes@gmail.com>
2023-08-04 19:03:21 +0300

    new UI for missing players notice

--- src/features/dashboard/highlighted-players/HighlightedPlayers.tsx
+++ src/features/dashboard/highlighted-players/HighlightedPlayers.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useMemo } from 'react';
 import DashboardContext from '../DashboardContext';
 import HighlightedPlayer from './HighlightedPlayer';
 import { getHighlightedPlayers } from './extractors';
+import MissingPlayersNotice from '@/features/missing-players/MissingPlayersNotice';
 
 const HighlightedPlayers = () => {
     const { userLeagueInfo, oppLeagueInfo } = useContext(DashboardContext);
@@ -11,7 +12,10 @@ const HighlightedPlayers = () => {
     const shouldShowHighlightedPlayers = Object.keys(highlightedPlayers).length >= 1;
     return (
         shouldShowHighlightedPlayers ? <div>
-            <p className='text-primary-text mb-2 text-lg font-semibold'>Spotlight Players</p>
+            <div className='flex gap-3 align-center'>
+                <p className='text-primary-text mb-2 text-lg font-semibold'>Spotlight Players</p>
+                <MissingPlayersNotice />
+            </div>
             <ul className='col-span-2 flex gap-3 overflow-scroll py-2 scrollbar-thin scrollbar-thumb-accent scrollbar-track-secondary-accent'>
                 {Object.entries(highlightedPlayers).map(([playerId, { hightlightedPlayerType, conflictScore }]) => (
                     <HighlightedPlayer key={playerId} playerId={playerId} playerHighlightType={hightlightedPlayerType} conflictScore={conflictScore} />
--- src/features/missing-players/MissingPlayersNotice.tsx
+++ src/features/missing-players/MissingPlayersNotice.tsx
@@ -2,27 +2,40 @@ import React, { useContext, useMemo } from 'react';
 import { useGetLocalStorage } from '../local-storage/hooks';
 import { getMissingPlayersDiff } from './getMissingPlayersDiff';
 import DashboardContext from '../dashboard/DashboardContext';
+import Tooltip from '@/components/Tooltip';
 
 
 const MissingPlayersNotice: React.FC = () => {
-    const {data: cachedSettings} = useGetLocalStorage('settings');
-    const {data: cachedLeaguesInfo} = useGetLocalStorage('leaguesInfo');
-    const {leagueNames, leagueStarterSpots} = cachedLeaguesInfo ?? {};
-    const {shouldShowMissingStarters} = cachedSettings ?? {};
-    const {userLeagueInfo: userStarters, playersInfo, scheduleData} = useContext(DashboardContext);
-    const {diffInStarters, isSomeDiff} = useMemo(() => getMissingPlayersDiff({userStarters, playersInfo, leagueStarterSpots, scheduleData}), [leagueStarterSpots]);
+    const { data: cachedSettings } = useGetLocalStorage('settings');
+    const { data: cachedLeaguesInfo } = useGetLocalStorage('leaguesInfo');
+    const { leagueNames, leagueStarterSpots } = cachedLeaguesInfo ?? {};
+    const { shouldShowMissingStarters } = cachedSettings ?? {};
+    const { userLeagueInfo: userStarters, playersInfo, scheduleData } = useContext(DashboardContext);
+    const { diffInStarters, isSomeDiff } = useMemo(() => getMissingPlayersDiff({ userStarters, playersInfo, leagueStarterSpots, scheduleData }), [leagueStarterSpots]);
 
+    // return shouldShowMissingStarters && isSomeDiff ? (
+    //     <div className='mb-4 text-primary-text bg-rose-500 rounded p-2'>
+    //         <h5 className='text-sm md:text-base'>⚠️ Notice !</h5>
+    //         <p className='text-xs md:text-sm mb-2'>You got some missing starters in your lineups:</p>
+    //         {Object.entries(diffInStarters).map(([leagueId, diff]) => {
+    //             return (diff !== 0) ?
+    //                 <li className='text-sm md:text-base list-none' key={leagueId}>{leagueNames?.[leagueId]} - {diff}</li>
+    //                 : null;
+    //         })}
+    //     </div>
+    // ) : null;
     return shouldShowMissingStarters && isSomeDiff ? (
-        <div className='mx-4 my-2 text-primary-text bg-rose-500 rounded p-2'>
-            <h5 className='text-sm md:text-base'>⚠️ Notice !</h5>
-            <p className='text-xs md:text-sm mb-2'>You got some missing starters in your lineups:</p>
-            {Object.entries(diffInStarters).map(([leagueId, diff]) => {
-                return (diff !== 0) ?
-                <li className='text-sm md:text-base list-none' key={leagueId}>{leagueNames?.[leagueId]} - {diff}</li> 
-                : null;
-            })}
-        </div>
-    ): null;
+        <Tooltip TooltipComponent={
+            <div className='text-primary-text bg-rose-500 rounded p-2 w-36 md:w-56 text-xs lg:text-sm'>
+                <p className='mb-2'>You got some missing starters in your lineups:</p>
+                {Object.entries(diffInStarters).map(([leagueId, diff]) => {
+                    return (diff !== 0) ?
+                        <li className='list-none' key={leagueId}>{leagueNames?.[leagueId]} - {diff}</li>
+                        : null;
+                })}
+            </div>
+        }><abbr>⚠️</abbr></Tooltip>
+    ) : null;
 };
 
 export default MissingPlayersNotice;
