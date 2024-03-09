19180ae414d0c5462678458a81549107479bc580
Guy Haviv <theguyhaviv@gmail.com>
2024-03-09 21:08:07 +0200

    compute is def player id

--- src/features/dashboard/player-modal/content-utils.ts
+++ src/features/dashboard/player-modal/content-utils.ts
@@ -1,5 +1,5 @@
 export const getPlayerImageById = (playerId: string | undefined) => {
-    const isDefPlayerId = isNaN(Number(playerId));
+    const isDefPlayerId = Number(playerId) < 0;
     const playerImageSrc = isDefPlayerId ? `https://sleepercdn.com/images/team_logos/nfl/${playerId?.toLowerCase()}.png` : `https://sleepercdn.com/content/nfl/players/${playerId}.jpg`
     return playerImageSrc;
 };
