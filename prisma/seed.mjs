ecaa18c234133f7ae31c216a6f2aa95e0d2f8993
Guy Haviv <theguyhaviv@gmail.com>
2024-03-10 19:58:41 +0200

    align DEF player IDs from DB (number) to team abv(string)

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -2,12 +2,48 @@ import fetch from 'node-fetch';
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
+const TEAM_ABV_TO_ID = {
+    ARI: -1, // Arizona Cardinals
+    ATL: -2, // Atlanta Falcons
+    BAL: -3, // Baltimore Ravens
+    BUF: -4, // Buffalo Bills
+    CAR: -5, // Carolina Panthers
+    CHI: -6, // Chicago Bears
+    CIN: -7, // Cincinnati Bengals
+    CLE: -8, // Cleveland Browns
+    DAL: -9, // Dallas Cowboys
+    DEN: -10, // Denver Broncos
+    DET: -11, // Detroit Lions
+    GB: -12, // Green Bay Packers
+    HOU: -13, // Houston Texans
+    IND: -14, // Indianapolis Colts
+    JAX: -15, // Jacksonville Jaguars
+    KC: -16, // Kansas City Chiefs
+    LV: -17, // Las Vegas Raiders
+    LAC: -18, // Los Angeles Chargers
+    LAR: -19, // Los Angeles Rams
+    MIA: -20, // Miami Dolphins
+    MIN: -21, // Minnesota Vikings
+    NE: -22, // New England Patriots
+    NO: -23, // New Orleans Saints
+    NYG: -24, // New York Giants
+    NYJ: -25, // New York Jets
+    PHI: -26, // Philadelphia Eagles
+    PIT: -27, // Pittsburgh Steelers
+    SF: -28, // San Francisco 49ers
+    SEA: -29, // Seattle Seahawks
+    TB: -30, // Tampa Bay Buccaneers
+    TEN: -31, // Tennessee Titans
+    WAS: -32,
+    WSH: -32
+}
+
 const extractDataFromApiResponse = (playersDataFromSleeper) => {
     const players = Object.values(playersDataFromSleeper);
     const playersRefinedInfo = players.map((player, index) => {
         const playerNumberId = Number(player.player_id);
         return {
-            id: Number.isNaN(playerNumberId) ? -index : playerNumberId,
+            id: Number.isNaN(playerNumberId) ? TEAM_ABV_TO_ID[player.player_id] : playerNumberId,
             firstName: player.first_name,
             lastName: player.last_name,
             position: player.position,
--- src/features/dashboard/player-modal/content-utils.ts
+++ src/features/dashboard/player-modal/content-utils.ts
@@ -1,5 +1,8 @@
+import { getTeamAbvFromDBId } from "@/features/teams/data-transfomers";
+
 export const getPlayerImageById = (playerId: string | undefined) => {
     const isDefPlayerId = Number(playerId) < 0;
-    const playerImageSrc = isDefPlayerId ? `https://sleepercdn.com/images/team_logos/nfl/${playerId?.toLowerCase()}.png` : `https://sleepercdn.com/content/nfl/players/${playerId}.jpg`
+    const teamAbvFromDBId = getTeamAbvFromDBId(Number(playerId));
+    const playerImageSrc = isDefPlayerId ? `https://sleepercdn.com/images/team_logos/nfl/${teamAbvFromDBId?.toLocaleLowerCase()}.png` : `https://sleepercdn.com/content/nfl/players/${playerId}.jpg`
     return playerImageSrc;
 };
