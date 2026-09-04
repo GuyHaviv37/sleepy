55744f9cc890ed628e680455e5d2c6f8797ad393
Guy Haviv <theguyhaviv@gmail.com>
2026-09-04 17:48:12 +0300

    hide mock drafts EP

--- src/pages/user/[id]/index.tsx
+++ src/pages/user/[id]/index.tsx
@@ -56,9 +56,9 @@ const UserDashboardPage = (props: { nflWeek: WEEKS }) => {
                 <Link href="/" passHref >
                     <PageLogo title={'🏈 Sleepy'} onClick={bi.logHomeLinkClicked} />
                 </Link>
-                <Link href={`/user/${id}/mock-drafts`} >
+                {/* <Link href={`/user/${id}/mock-drafts`} >
                     <button className='text-md text-primary-text font-semibold absolute top-5 right-28 md:right-32' onClick={bi.logMockDraftsLinkClicked}>🔮 Mock Drafts</button>
-                </Link>
+                </Link> */}
                 <Link href={`/user/${id}/settings`} >
                     <button className='text-md text-primary-text font-semibold absolute top-5 right-5' onClick={bi.logSettingsLinkClicked}>⚙️ Settings</button>
                 </Link>
